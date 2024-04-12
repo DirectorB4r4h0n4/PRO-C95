9020a16f9916def00846a2f034a4af115b6a43cb
DirFranck <123032605+DirectorB4r4h0n4@users.noreply.github.com>
2024-04-12 18:15:22 -0500

    Add files via upload

---
+++ PRO-C95/Screens/CreatePost.js
@@ -0,0 +1,13 @@
+// CreatePost.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const CreatePost = () => {
+  return (
+    <View>
+      <Text>Crear publicación</Text>
+    </View>
+  );
+};
+
+export default CreatePost;
---
+++ PRO-C95/Screens/Profile.js
@@ -0,0 +1,13 @@
+// Profile.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Profile = () => {
+  return (
+    <View>
+      <Text>Perfil</Text>
+    </View>
+  );
+};
+
+export default Profile;
